ed2ac0c36ac7971f2a56fa53fe552ba01f429422
Mert Ziya <108177583+mertziya@users.noreply.github.com>
2023-12-09 02:03:11 +0300

    recommendation backend integration

---
+++ lib/models/recommendationModel.dart
@@ -0,0 +1,20 @@
+class RecommendationModel {
+  final String songName;
+  final String mainArtistName;
+
+  final String albumImg;
+
+  RecommendationModel({
+    required this.songName,
+    required this.mainArtistName,
+    required this.albumImg,
+  });
+
+  factory RecommendationModel.fromJson(Map<String, dynamic> json) {
+    return RecommendationModel(
+      songName: json['songName'],
+      mainArtistName: json['mainArtistName'],
+      albumImg: json['albumImg'],
+    );
+  }
+}
--- lib/pages/recommendations.dart
+++ lib/pages/recommendations.dart
@@ -1,7 +1,63 @@
 import 'package:flutter/material.dart';
+import '../apis/recommendationsLogic.dart';
+import '../models/recommendationModel.dart';
 
 class RecommendationsPage extends StatelessWidget {
   const RecommendationsPage({Key? key}) : super(key: key);
+
+  Widget _buildRecommendationsList(
+      Future<List<RecommendationModel>> recommendationsFuture) {
+    return FutureBuilder<List<RecommendationModel>>(
+      future: recommendationsFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error.toString()}',
+              style: const TextStyle(color: Colors.white));
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return const Text('No recommendations available',
+              style: TextStyle(color: Colors.white));
+        }
+
+        return SizedBox(
+          height: 180.0,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              var recommendation = snapshot.data![index];
+              return Container(
+                width: 130.0,
+                margin: const EdgeInsets.only(right: 15.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.grey[800],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Image.network(recommendation.albumImg, height: 80.0),
+                    Text(
+                      recommendation.songName,
+                      style: const TextStyle(color: Colors.white),
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      recommendation.mainArtistName,
+                      style: const TextStyle(color: Colors.white),
+                      textAlign: TextAlign.center,
+                    ),
+                    // Additional information from RecommendationModel can be displayed here if needed
+                  ],
+                ),
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
 
   Widget _buildSectionTitle(String title) {
     return Padding(
@@ -17,69 +73,45 @@ class RecommendationsPage extends StatelessWidget {
     );
   }
 
-  Widget _buildHorizontalList() {
-    // Dummy items for the horizontal list
-    List<Widget> items = List.generate(10, (index) {
-      return Container(
-        width: 130.0,
-        margin: const EdgeInsets.only(right: 15.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-          color: Colors.grey[800],
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: const [
-            // Replace with actual data
-            FlutterLogo(size: 80.0),
-            Text(
-              'Item',
-              style: TextStyle(color: Colors.white),
-              textAlign: TextAlign.center,
-            ),
-          ],
-        ),
-      );
-    });
-
-    return SizedBox(
-      height: 180.0,
-      child: ListView(
-        scrollDirection: Axis.horizontal,
-        children: items,
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF171717),
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-        title: const Text('Recommendations'),
-        centerTitle: true,
-      ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
+              // BASED ON SONGS
               _buildSectionTitle('Based on Songs'),
-              _buildHorizontalList(),
-              _buildSectionTitle('Based on Artists'),
-              _buildHorizontalList(),
+              _buildRecommendationsList(
+                  fetchRecommendations(RecommendationType.song)),
+
+              // BASED ON ALBUMS
               _buildSectionTitle('Based on Albums'),
-              _buildHorizontalList(),
+              _buildRecommendationsList(
+                  fetchRecommendations(RecommendationType.album)),
+
+              //BASED ON ARTISTS
+              _buildSectionTitle('Based on Arists'),
+              _buildRecommendationsList(
+                  fetchRecommendations(RecommendationType.artist)),
+
+              // BASED ON SPOTIFY
               _buildSectionTitle('Based on Spotify'),
-              _buildHorizontalList(),
+              _buildRecommendationsList(
+                  fetchRecommendations(RecommendationType.spotify)),
+
+              // BASED ON TEMPORAL
+              _buildSectionTitle('Based on Temporal'),
+              _buildRecommendationsList(
+                  fetchRecommendations(RecommendationType.temporal)),
+
+              // BASED ON FRIENDS
               _buildSectionTitle('Based on Friends'),
-              _buildHorizontalList(),
-              _buildSectionTitle('Based on Temporal Values'),
-              _buildHorizontalList(),
+              _buildRecommendationsList(
+                  fetchRecommendations(RecommendationType.friends)),
             ],
           ),
         ),
